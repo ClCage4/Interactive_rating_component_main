547bc3c278ffb00022c7bbe50a2bfe8f6f06ec11
ClCage4 <112894479+ClCage4@users.noreply.github.com>
2022-10-22 21:54:02 -0500

    created rating eventlistener

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -2,21 +2,20 @@ const mainContainer = document.querySelector(".container");
 const thanksContainer = document.querySelector(".thank-you");
 const submitButton = document.getElementById("submit-rating");
 const rateAgain = document.getElementById("rate-again");
-const rating = document.querySelectorAll(".btn");
+const ratings = document.querySelectorAll(".btn");
 const rates = document.getElementById("rating");
 
 submitButton.addEventListener("click", () => {
   mainContainer.style.display = "none";
   thanksContainer.classList.remove("hidden");
 });
-rating.forEach((rating) => {
+ratings.forEach((rating) => {
   rating.addEventListener("click", () => {
-    console.log(rate.innerHTML);
-    //rating.innerHTML = rate.innerHTML;
+    rating.innerHTML = rating.innerHTML;
   });
 
   rateAgain.addEventListener("click", () => {
-    mainContainer.style.display = "block";
     thanksContainer.classList.add("hidden");
+    mainContainer.style.display = "block";
   });
 });
